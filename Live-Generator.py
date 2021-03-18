7fa7d9c8cdb94c202907cb922031cf8a52e93c35
Scorpio28-Oficial <77907586+Scorpio28-Oficial@users.noreply.github.com>
2021-03-17 20:09:43 -0600

    Update Live-Generator.py
    # Live-Generator
    
    __Esta es una herramienta creada en python cuya función es generar lives de bins. Dicha herramienta fue creada con fines educativos e informativos, Scorpio28 no se responsabiliza por el mal uso__
    
    __SALUDOS...__
    
    Unate a nuestra gran comunidad Informatic in Termux.
    
    __🦂 INSTALACION: 🦂__
    
    apt update && apt upgrade -y
    
    pkg install python -y
    
    pkg install git -y
    
    git clone https://github.com/Scorpio28-Oficial/Live-Generator
    
    cd Live-Generator
    
    ls
    
    chmod 777 Live-Generator.py
    
    ls
    
    python Live-Generator.py
    
    __Una vez en la interfax aparece un ejemplo de ejecución bastante sencillo.__
    
    # By: Scorpio28
    
    __Informatic in Termux__
    
    https://t.me/Informatic_in_Termux_1
    
    
    __Esta información es solo con fines educativos e informativos, el mal uso es responsabilidad de quien la utilize.__
    
    # EL CONOCIMIENTO ES LIBRE.
    
    __NO EXPLICO NI DOY CURSITOS DE CARDING.__
    
    __SOY ÉTICO ESTA HERRAMIENTA  ES PARA USO EDUCATIVO UNICAMENTE EL MAL USO DE LA MISMA ES BAJO TU RESPONSABILIDAD.__

--- Live-Generator.py
+++ Live-Generator.py
@@ -10,7 +10,7 @@ from random import randint
 version = "1.0.0"
 #Mensaje
 os.system ("clear")
-print("\033[1;36m Live-Generator (GENERADOR DE BINS)")
+print("\033[1;32m+\033[1;36m LIVE-GENERATOR")
 time.sleep(1.2)
 #Informacion de ayuda
 def dexter1999():
